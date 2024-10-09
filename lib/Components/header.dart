02bdbdbb1c5036aa146dc62a4bfa16b3ba9c645c
Fabio Recchia <fabio.recchia@edu.itspiemonte.it>
2024-10-09 10:13:42 +0200

    09/10/2024 10:13

---
+++ lib/Components/header.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  final bool isFlashOn;
+  final VoidCallback onFlashToggle;
+
+  const Header({
+    super.key,
+    required this.isFlashOn,
+    required this.onFlashToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xFF092d52).withOpacity(0.9),
+      child: Column(
+        children: [
+          SafeArea(
+            bottom: false,
+            child: Container(height: 0),
+          ),
+          SizedBox(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.white),
+                  onPressed: () {
+                    // Menu functionality
+                  },
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        isFlashOn ? Icons.flash_on : Icons.flash_off,
+                        color: Colors.white,
+                      ),
+                      onPressed: onFlashToggle,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.photo_library, color: Colors.white),
+                      onPressed: () {
+                        // Photo library functionality
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
