404c2702d79e08e5560f00099a19ff073ab0d0c6
DavideArezzi <davide.arezzi@edu.itspiemonte.it>
2024-10-16 09:37:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'login_selmi.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ScanSelmi',
+      theme: ThemeData(
+        primaryColor: Color(0xFF304A78),
+        scaffoldBackgroundColor: Color(0xFF304A78),
+        fontFamily: 'Roboto',  // Assicurati di aver configurato questo font nel pubspec.yaml
+      ),
+      home: LoginPage(),
+      debugShowCheckedModeBanner: false,  // Rimuove il banner di debug
+    );
+  }
+}
+
+
